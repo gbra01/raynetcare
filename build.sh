a2efa18875346fe3659ef2dccde0d5a073b3eb58
Ray Akuaku <ray.akuaku@vanderlande.com>
2026-01-30 14:59:22 +0000

    Fix build.sh to LF for Render

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
+@'
 #!/usr/bin/env bash
 set -o errexit
 
@@ -6,3 +7,7 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --noinput
 python manage.py migrate
+'@ | Set-Content -NoNewline build.sh
+
+# Convert to LF only (remove any CR characters)
+(Get-Content .\build.sh -Raw) -replace "`r","" | Set-Content -NoNewline .\build.sh
